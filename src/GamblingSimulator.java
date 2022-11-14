1c6ec4c126b8dfc8e07a5d3c6e29389a3fb9f069
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-11-15 02:04:56 +0530

    UC2  uploading

--- src/GamblingSimulator.java
+++ src/GamblingSimulator.java
@@ -1,13 +1,41 @@
 
 public class GamblingSimulator {
-	public int stake = 100;
-	public int bet = 1;
-	
+	public int oneDayGamestake = 100;
+	public int perGameBet = 1;
+	public int gameNumber = 0;
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("This is  the Gambling Simulator!!!");
-		System.out.println("Gambler start with a stake  100" );
-		
+		System.out.println("Gambler start with a stake  100");
+
+		GamblingSimulator gambling = new GamblingSimulator();
+		gambling.playGame();
+	}
+
+	public void playGame() {
+		int winStack = 0;
+		int looseStack = 0;
+		int isGameWinOrLoose = (int) (Math.random() * 2);
+
+		switch (isGameWinOrLoose) {
+		case 0:
+			System.out.println("Gambler loose the game");
+			gameNumber++;
+			looseStack = looseStack + perGameBet;
+			System.out.println(looseStack);
+			oneDayGamestake = oneDayGamestake - looseStack;
+			break;
+		case 1:
+			System.out.println("Gambler win the game");
+			gameNumber++;
+			winStack = winStack + perGameBet;
+			System.out.println(winStack);
+			oneDayGamestake = oneDayGamestake + winStack;
+			break;
+		}
+		System.out.println("Game Stack : " + oneDayGamestake);
+
 	}
 
 }
