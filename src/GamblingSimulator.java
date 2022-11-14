648a600aa448c39e071ce49fba503870b76cd70a
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-11-15 01:20:36 +0530

    UC1 information upload

--- src/GamblingSimulator.java
+++ src/GamblingSimulator.java
@@ -1,10 +1,13 @@
 
 public class GamblingSimulator {
-
+	public int stake = 100;
+	public int bet = 1;
+	
 	public static void main(String[] args) {
-
+		
 		System.out.println("This is  the Gambling Simulator!!!");
-
+		System.out.println("Gambler start with a stake  100" );
+		
 	}
 
 }
